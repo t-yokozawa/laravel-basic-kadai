a0334e766cbfb35aa49aa6886cf9ea04c1048212
yokozawa <yokozawa.takumi@sannichi-ybs.co.jp>
2025-05-07 09:42:18 +0900

    Eloquent ORM課題

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Posts;
 
 class PostController extends Controller
 {
     public function index(){
         $posts = DB::table('posts')->get();
         return view('posts.index', compact('posts'));
+    }
+
+    public function show($id){
+        $posts = Posts::find($id);
+        return view('posts.show', compact('posts'));
     }
 }
