1012216ee3a3e3d75fe2b8ba87dd14d155d4c933
yokozawa <yokozawa.takumi@sannichi-ybs.co.jp>
2025-05-08 11:56:22 +0900

    データの作成機能とバリデーションの課題

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Posts;
@@ -10,11 +11,30 @@ class PostController extends Controller
 {
     public function index(){
         $posts = DB::table('posts')->get();
+        // $posts = Post::all();
         return view('posts.index', compact('posts'));
     }
 
     public function show($id){
         $posts = Posts::find($id);
         return view('posts.show', compact('posts'));
+    }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store (Request $request){
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+        
+        $posts = new Posts();
+        $posts->title = $request->input('title');
+        $posts->content = $request->input('content');
+        $posts->save();
+
+        return redirect('/posts');
     }
 }
